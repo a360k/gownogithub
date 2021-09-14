d2fc020536f6ca6275c7f1b195be18a1eaabc135
ms881 <polak23123@wod.re>
2021-09-14 20:15:58 +0200

    tests

---
+++ SpecTest1.feature
@@ -0,0 +1,15 @@
+﻿Feature: SpecTest1
+	Simple calculator for adding two numbers
+
+@mytag
+Scenario Outline: Add two numbers
+	Given the first number is '<a>' and total is '<total>'
+
+	Examples:
+	| a | total |
+	| 1 | 101   |
+	| 2 | 102   |
+	| 3 | 103   |
+	| 4 | 104   |
+	| 5 | 105   |
+	| 6 | 106   |
---
+++ StepDefinition1.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace NUnitTestProject1
+{
+    [Binding]
+    public sealed class StepDefinition1
+    {
+        private readonly ScenarioContext _scenarioContext;
+
+        public StepDefinition1(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given(@"the first number is '(.*)' and total is '(.*)'")]
+        public void GivenTheFirstNumberIsAndTotalIs(int p0, int p1)
+        {
+            var total = p0 + 100;
+            Assert.AreEqual(p1, total);
+        }
+    }
+}
